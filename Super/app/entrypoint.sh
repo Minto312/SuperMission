c352336102992d299219f57486078e4bccfc71e9
Minto312 <karinto3219@gmail.com>
2024-02-24 01:01:25 +0000

    specified name in make migrations

--- Super/app/entrypoint.sh
+++ Super/app/entrypoint.sh
@@ -1,6 +1,6 @@
 cd /app
 
-python3 manage.py makemigrations --noinput
+python3 manage.py makemigrations account app gallery --noinput
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
 
